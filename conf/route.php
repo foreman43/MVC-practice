14bb16320c818be4fb017c27265aeb17e635d618
Артём Лукьянов <a-lukyanov@bk.ru>
2023-07-12 16:00:25 +0400

    created initial Routing class

--- conf/route.php
+++ conf/route.php
@@ -9,5 +9,32 @@
 
 class Routing
 {
+    public static function buildRoute()
+    {
+        $controllerName = "IndexController";
+        $action = "IndexAction";
+        $modelName = "IndexModel";
 
+        $route = explode('/', $_SERVER['REQUEST_URI']);
+
+        if($route[1] != '') {
+            $controllerName = ucfirst($route[1]. "Controller");
+            $modelName = ucfirst($route[1]. "Model");
+        }
+
+        include CONTROLLER_PATH . $controllerName . ".php";
+        include MODEL_PATH . $controllerName . ".php";
+
+        if(isset($route[2])) {
+            $action = $route[2];
+        }
+
+        $controller = new $controllerName();
+        $controller->$action();
+    }
+
+    public static function showError()
+    {
+
+    }
 }
