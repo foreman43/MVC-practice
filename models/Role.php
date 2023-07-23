cbaa92d4d33f1753dc03ed94ea4ce24ef00543b1
Артём Лукьянов <a-lukyanov@bk.ru>
2023-07-23 15:02:37 +0400

    Created ActiveRecord for role table

---
+++ models/Role.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\models;
+
+use app\core\ActiveRecord;
+
+class Role extends ActiveRecord
+{
+    public static function tableName(): string
+    {
+        return 'role';
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'id',
+            'name'
+        ];
+    }
+
+    public static function primaryKey(): string
+    {
+        return 'id';
+    }
+
+    public function rules(): array
+    {
+        return [];
+    }
+
+    public function getLabels(): array
+    {
+        return [
+            'id' => 'Identifier',
+            'name' => 'Role name'
+        ];
+    }
+}
