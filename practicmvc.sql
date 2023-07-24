de604690b7373fed2111784dc9993f6b031636a0
Артём Лукьянов <a-lukyanov@bk.ru>
2023-07-24 14:28:28 +0400

    added DB dump

---
+++ practicmvc.sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Июл 24 2023 г., 12:26
+-- Версия сервера: 10.4.25-MariaDB
+-- Версия PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `practicmvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `theme_id` tinyint(3) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `response_required` tinyint(1) NOT NULL DEFAULT 0,
+  `heading` varchar(30) NOT NULL,
+  `text` text NOT NULL,
+  `send_to` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `theme_id`, `user_id`, `response_required`, `heading`, `text`, `send_to`) VALUES
+(14, 1, 12, 1, 'dsddfs', 'fdsfdsf', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `role`
+--
+
+CREATE TABLE `role` (
+  `id` tinyint(3) UNSIGNED NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `role`
+--
+
+INSERT INTO `role` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'manager'),
+(3, 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `themes`
+--
+
+CREATE TABLE `themes` (
+  `id` tinyint(3) UNSIGNED NOT NULL,
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `themes`
+--
+
+INSERT INTO `themes` (`id`, `name`) VALUES
+(1, 'Ошибка'),
+(2, 'Жалоба');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `password` varchar(100) NOT NULL,
+  `role_id` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `name`, `password`, `role_id`) VALUES
+(4, 'dsf@gmail.com', '', '$2y$10$GZf1kc/EHO5V470oImnkvOLsV', 3),
+(5, 'dsef@gmail.com', '', '$2y$10$0vs3kk2VPqrYdNWg17VGY.z.N', 3),
+(6, 'dsesf@gmail.com', '', '$2y$10$63zOfnIYoAjAtn1kXTj7JOwoq', 3),
+(7, 'ddsef@gmail.com', '', '$2y$10$q7l4vLHWKICfuvMrCQFCj.MuF', 3),
+(8, 'ddssef@gmail.com', '', '$2y$10$XB1kR9k9iOCuujWx8D8BiOS2Q', 3),
+(9, 'dasaf@gmail.com', '', '$2y$10$uNbseJASDs3cME77D6uX0e/cE', 3),
+(12, 'a-lukyanov@bk.ru', 'test', '$2y$10$hYPyMI7cO0oJR0bjYdV/wOz6pihaPPFT49xbiDtyh9aoI23Lx4NaO', 3);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `theme_id` (`theme_id`),
+  ADD KEY `send_to` (`send_to`),
+  ADD KEY `feedback_ibfk_2` (`user_id`);
+
+--
+-- Индексы таблицы `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `themes`
+--
+ALTER TABLE `themes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT для таблицы `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `themes`
+--
+ALTER TABLE `themes`
+  MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`theme_id`) REFERENCES `themes` (`id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `feedback_ibfk_3` FOREIGN KEY (`send_to`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
